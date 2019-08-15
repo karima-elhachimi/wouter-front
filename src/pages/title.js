f4242ac570d049e36daaed0017f5bdf0402fb6ac
karima-elhachimi <yuakarima@icloud.com>
2019-08-15 16:41:49 +0200

    imported font-family

--- src/pages/title.js
+++ src/pages/title.js
@@ -2,10 +2,7 @@ import React from "react"
 import "../css/title.css"
 
 export default () => (
- 
-    <link href={`https://fonts.googleapis.com/css?family=Julius+Sans+One|Montserrat&display=swap`} rel="stylesheet"/>
-<div className="title-tag" >
-    
+<div className="title-tag" > 
     <h1 className="title-name">Wouter Bloemen</h1>
     <p className="title-sub">Coach and Maker</p>
 </div>
