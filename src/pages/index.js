b338e4b6c2d9558aa90adb941d03ca7483e98aca
karima-elhachimi <yuakarima@icloud.com>
2019-08-15 16:26:11 +0200

    created index and title component and added fullscreen video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import './index.css';
+import Title from "./title"
+import backgroundVideo from '../../static/background.mp4';
 
-export default () => <div>Hello bitches!</div>
+export default () => (
+    
+<div className="background">
+<video className='videoTag' autoPlay loop muted>
+    <source src={backgroundVideo} type='video/mp4' />
+</video>
+    <Title/>
+</div>
+)
---
+++ src/pages/title.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./title.css"
+
+export default () => (
+<div className="title-tag" >
+    <h1 className="title-name">Wouter Bloemen</h1>
+    <p className="title-sub">Coach and Maker</p>
+</div>
+)
