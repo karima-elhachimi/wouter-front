e6dc5e12e9728afdf56e51c02624339e4b336f5f
karima-elhachimi <yuakarima@icloud.com>
2019-08-18 12:48:32 +0200

    rebuild gatsby

--- src/html.js
+++ src/html.js
@@ -12,7 +12,6 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
